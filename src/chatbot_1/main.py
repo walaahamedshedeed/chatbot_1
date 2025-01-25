27125ccab4f10d3c5f24e53c5fc22831e105f2f1
Walaa <new_walaa@yahoo.co.uk>
2025-01-25 20:08:24 +0400

    Refactor main.py to enhance user interaction and memory management; add timing for response generation

--- src/chatbot_1/main.py
+++ src/chatbot_1/main.py
@@ -3,12 +3,21 @@ import sys
 import warnings
 from datetime import datetime
 from chatbot_1.crew import Chatbot1
-
+import time
 
 from mem0 import Memory
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+# from RealtimeSTT import AudioToTextRecorder
+# from elevenlabs import play
+# from elevenlabs.client import ElevenLabs
+import os
+
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+
 
 config = {
     "vector_store": {
@@ -30,29 +39,63 @@ warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 # interpolate any tasks and agents information
 
 def run():
+    print("Wait until it says 'speak now'")
+    
+    # client = ElevenLabs(
+    #     api_key=ELEVENLABS_API_KEY, # Defaults to ELEVEN_API_KEY or ELEVENLABS_API_KEY
+    # )
+    # recorder = AudioToTextRecorder(language="en")
     while True:
         user_input = input("You: ")
-        if user_input.lower() in ["exit", "quit", "bye"]:
-            print("Chatbot: Goodbye! It was nice talking to you.")
-            break
+        try:
+            # text = recorder.text()
+        
+            # print(text)
+            # recorder.stop()
+            # user_input = text
+            if user_input.lower() in ["exit", "quit", "bye"]:
+                print("Chatbot: Goodbye! It was nice talking to you.")
+                break
+            
+            start_time = time.time()
+            print("adding  user message to memory")
+            # Add user input to memory
+            memory.add(f"User: {user_input}", user_id="Lennex")
 
-        # Add user input to memory
-        memory.add(f"User: {user_input}", user_id="Lennex")
+            print("searching memory")
+            # Retrieve relevant information from vector store
+            relevant_info = memory.search(query=user_input,user_id="Lennex", limit=3)
+            context = "\\n".join(message["memory"] for message in relevant_info)
+            print(context)
 
-        # Retrieve relevant information from vector store
-        relevant_info = memory.search(query=user_input,user_id="Lennex", limit=3)
-        context = "\\n".join(message["memory"] for message in relevant_info)
-        print(context)
-        inputs = {
-            "user_message": f"{user_input}",
-            "context": f"{context}. The current time is :  {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}",
-        }
-    
-        response = Chatbot1().crew().kickoff(inputs=inputs)
+            inputs = {
+                "user_message": f"{user_input}",
+                "context": f"{context} The current time is :  {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}, today is {datetime.now().strftime("%A")}",
+            }
 
-        # Add chatbot response to memory
-        memory.add(f"Assistant: {response}", user_id="Assistant")
-        print(f"Assistant: {response}")
+            print("inputs",inputs)
+        
+            print("calling chatbot")
+            response = Chatbot1().crew().kickoff(inputs=inputs)
+            print(f"Time taken: {time.time() - start_time} seconds")
+
+            # Add chatbot response to memory
+            print(f"Assistant: {response} - {type(response)}")
+            
+            # audio = client.generate(
+            #     text=str(response),
+            #     voice="Mark - Natural Conversations",
+            #     model="eleven_flash_v2_5",
+            # )
+
+            # play(audio)
+            memory.add(f"Assistant: {response}", user_id="Assistant")
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            # print("Restarting recording...")
+            # time.sleep(1)
+            # continue
+
 
 
 def train():
